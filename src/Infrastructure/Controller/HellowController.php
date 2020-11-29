e704c157c4cc6788296cb3f592101817fa5a3b1a
yaman0 <yaman0@protonmail.com>
2020-11-29 11:30:10 +0100

    :rocket: setup CI for deploy
    - adding github action workflow trigger on main for to deploy app
    - adding helloworld controller to test heroku deployement
    - setup project for heroku
    
    refered to #2

---
+++ src/Infrastructure/Controller/HellowController.php
@@ -0,0 +1,22 @@
+<?php
+
+
+namespace App\Infrastructure\Controller;
+
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+/**
+ * Class HellowController
+ * @package App\Infrastructure\Controller
+ * @Route("/", name="index")
+ */
+class HellowController extends AbstractController
+{
+    public function __invoke(): Response
+    {
+        return new Response('Hello world !');
+    }
+}
